74442b54ba02e2dcca6d4403c79804a67d8248a0
arzaan <xdarzanplug@gmail.com>
2024-02-05 12:18:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6400468647:AAHd3WGvsIQWg47kBijOuxE1aM_8H1WUuFI"
-    API_ID = "10551685"
-    API_HASH = "477dff815fdc7cba803c1bc02c459d03"
-    AUTH_USERS = "1112773045,1975696269"
+    BOT_TOKEN = "6913163552:AAHy8Tmi-HYELZkViq0rUVLQ7cTWjYJoiMk"
+    API_ID = "4942197"
+    API_HASH = "3248a2c551b73193969b42194023635"
+    AUTH_USERS = "5892781710"
 
